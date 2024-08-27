99d023b5e368a54fa1c73ea26f4bf120afec072f
viji777 <152005431+viji777@users.noreply.github.com>
2024-08-27 15:18:47 +0530

    Update AddProduct.jsx

--- admin/src/Components/AddProduct.jsx
+++ admin/src/Components/AddProduct.jsx
@@ -27,7 +27,7 @@ const AddProduct = () => {
     let formData = new FormData();
     formData.append("product", image);
 
-    await fetch("http://localhost:4000/upload", {
+    await fetch("https://merchanza-bend.onrender.com/upload", {
       method: "POST",
       headers: {
         Accept: "application/json",
@@ -41,7 +41,7 @@ const AddProduct = () => {
     if (responseData.success) {
       product.image = responseData.image_url;
       console.log(product);
-      await fetch("http://localhost:4000/addproduct", {
+      await fetch("https://merchanza-bend.onrender.com/addproduct", {
         method: "POST",
         headers: {
           Accept: "application/json",
