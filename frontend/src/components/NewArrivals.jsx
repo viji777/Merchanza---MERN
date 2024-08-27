e81bf5eecf90c25d3dd4ef4633445fb153df2a3f
viji777 <152005431+viji777@users.noreply.github.com>
2024-08-27 15:11:04 +0530

    Update NewArrivals.jsx

--- frontend/src/components/NewArrivals.jsx
+++ frontend/src/components/NewArrivals.jsx
@@ -6,7 +6,7 @@ const NewArrivals = () => {
   const [new_collection, setNew_collection] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:4000/newcollections")
+    fetch("https://merchanza-bend.onrender.com/newcollections")
       .then((response) => response.json())
       .then((data) => setNew_collection(data));
   },[]);
